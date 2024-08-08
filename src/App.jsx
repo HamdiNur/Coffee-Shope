2325c21645003ec3046bb34cf2f9f140c44aa0e0
HamdiNur <hamdoonur26@gmail.com>
2024-08-08 19:19:56 +0300

    app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,28 @@
-// import React, { useEffect } from 'react';
-// import AOS from 'aos'
-// import "aos/dist/aos.css"
+import React, { useEffect } from 'react';
+import AOS from 'aos'
+import "aos/dist/aos.css"
 // import Navbar from './components/Navbar/Navbar';
-// import Home from './components/Home/Home';
+import Home from './components/Home/Home';
 // import Services from './components/Services/Services';
-// const App = () => {
-//   useEffect(()=>{
-//     AOS.init({
-//       offset:100,
-//       duration:700,
-//       easing: "ease-in",
-//       delay:100,
-//     });
+const App = () => {
+  useEffect(()=>{
+    AOS.init({
+      offset:100,
+      duration:700,
+      easing: "ease-in",
+      delay:100,
+    });
 
-//   });
-//   return (
-//     <div className="overflow-x-hidden"> 
-//            <Navbar/>
-//            <Home/>
-//            <Services/>
+  });
+  return (
+    <div className="overflow-x-hidden"> 
+           {/* <Navbar/> */}
+           <Home/>
+           {/* <Services/> */}
            
       
-//     </div>
-//   );
-// }
+    </div>
+  );
+}
 
-// export default App;
+export default App;
