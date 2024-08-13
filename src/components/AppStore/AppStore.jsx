9dc577e064f6566817950d377b9efac2d0618495
HamdiNur <hamdoonur26@gmail.com>
2024-08-13 20:22:40 +0300

    added some animation

--- src/components/AppStore/AppStore.jsx
+++ src/components/AppStore/AppStore.jsx
@@ -17,23 +17,17 @@ const AppStore = () => {
    <>
    <div   style={backgroundStyle} className='py-14'>
     <div className="container">
-        <div className="grid grid-cols-1 sm:grid-cols-2
-        items-center gap-4">
-            <div  className='space-y-6 max-w-xl mx-auto'> 
+        <div className="grid grid-cols-1 sm:grid-cols-2   items-center gap-4">
+            <div  data-aos="fade-up"  className='space-y-6 max-w-xl mx-auto'> 
                 {/* Text Content */}
-                  <h1 className='text-2xl
-                  text-center sm:text-left sm:text-4xl
-                  font-semibold  pl-3 text-white/90  
-                 
-                 '>Coffee  Cafe is availabe for Android and IOS</h1>
+                  <h1 className='text-2xl   text-center sm:text-left sm:text-4xl  font-semibold  pl-3 text-white/90  
+                    '>Coffee  Cafe is availabe for Android and IOS</h1>
            
                  {/* logo section */}
-                 <div className='flex flex-wrap justify-center 
-                 sm:justify-start items-center'>
+                 <div className='flex flex-wrap justify-center   sm:justify-start items-center'>
                     <a href="#">
                         <img src={AppStoreImg} alt="" 
-                         className='max-w-[150px] sm:max-w-[120px]
-                         md:max-w-[200px]'
+                         className='max-w-[150px] sm:max-w-[120px]    md:max-w-[200px]'
                         />
                      
                     </a>
